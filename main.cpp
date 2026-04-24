17c356203b433bdef2cc628bbad0a1b380f2c187
Oleksandr Krasichkov <alexkrasichkov@gmail.com>
2026-04-24 20:04:27 +0200

    virtual methods-getters added to class Transport

--- main.cpp
+++ main.cpp
@@ -4,8 +4,11 @@ class Transport()
 private:
         std::string typeOfFuel;
         double amountOfFuel;
-        double speed;
-        
+        double speed;        
 public:
+        virtual std::string getTypeOfFuel(){return typeOfFuel}
+        virtual double getAmountOfFuel(){return amountOfFuel}
+        virtual double getSpeed(){return speed}
+
         
 }
