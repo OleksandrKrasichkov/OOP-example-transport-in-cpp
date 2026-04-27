9ecf1affd14cb1edc064d4e1628dddf6b8fa61c4
Oleksandr Krasichkov <alexkrasichkov@gmail.com>
2026-04-27 22:11:02 +0200

    Tests for Tesla added

--- tests.cpp
+++ tests.cpp
@@ -12,7 +12,7 @@ void test_Toyota()
         bool thrown = false;
         try{
                 Toyota t1("Crow",-10, -1);
-        }catch (const std::runtime_error&) {
+        }catch (const std::runtime_error &e) {
                 thrown = true;
         }
         assert(thrown);
@@ -23,6 +23,22 @@ void test_Toyota()
 
         delete t2;
 }
+void test_Tesla()
+{
+        Tesla t1;
+        assert(t1.getTypeOfFuel() == "Electric");
+        bool thrown = false;
+        try
+        {Tesla t2(-1);}
+        catch (const std::runtime_error &e){thrown = true;}
+        assert(thrown);
+        Car *t2 = new Tesla();
+        assert(t2->hoursBeforeEmpty() - 4.44 < 0.01);
+        assert(t2->moneyPerTime(10)-198 < 0.01);
+        assert(t2->wageToPersonnel() == 1000);
+        delete t2;
+}
+
 
 
 
@@ -31,6 +47,7 @@ int main()
         std::cout<<std::fixed<<std::setprecision(2);
 
         test_Toyota();
+        test_Tesla();
 return 0;
 }
 
