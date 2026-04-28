84cd56800c27cb6a27e802f647fe74fd08da1a46
Oleksandr Krasichkov <alexkrasichkov@gmail.com>
2026-04-28 13:43:35 +0200

    Test for Ferry added

--- tests.cpp
+++ tests.cpp
@@ -26,7 +26,12 @@ void test_Toyota()
 void test_Tesla()
 {
         Tesla t1;
-        assert(t1.getTypeOfFuel() == "Electric");
+        assert(t1.getBatteryCapacity() == 80);
+        Tesla t11;
+        t11.setBatteryCapacity(1000);
+        assert (t11.getBatteryCapacity() == 1000);
+        t1 = t11;
+        assert (t1.getBatteryCapacity() == 1000);
         bool thrown = false;
         try
         {Tesla t2(-1);}
@@ -42,6 +47,11 @@ void test_Jet()
 { 
         Jet t1;
         assert(t1.getTypeOfFuel() == "Kerosene");
+        Jet t11;
+        t11.setTypeOfFuel("Diesel");
+        assert (t11.getTypeOfFuel() == "Diesel");
+        t1 = t11;
+        assert (t1.getTypeOfFuel() == "Diesel");
         bool thrown = false;
         try
         {Jet t2(1, -1);}
@@ -53,7 +63,27 @@ void test_Jet()
         assert(t2->wageToPersonnel() == 6000);
         delete t2;
 }
+void test_Ferry()
+{
 
+        Ferry t1;
+        assert(t1.getTypeOfFuel() == "Diesel");
+        Ferry t11;
+        t11.setTypeOfFuel("Gasoline");
+        assert (t11.getTypeOfFuel() == "Gasoline");
+        t1 = t11;
+        assert (t1.getTypeOfFuel() == "Gasoline");
+        bool thrown = false;
+        try
+        {Ferry t2(-1);}
+        catch (const std::runtime_error &e){thrown = true;}
+        assert(thrown);
+        WaterTransport *t2 = new Ferry();
+        assert(t2->hoursBeforeEmpty() - 33.33 < 0.01);
+        assert(t2->moneyPerTime(10)-5250 < 0.01);
+        assert(t2->wageToPersonnel()-18000 < 0.01);
+        delete t2;
+}
 
 
 
@@ -64,6 +94,7 @@ int main()
         test_Toyota();
         test_Tesla();
         test_Jet();
+        test_Ferry();
 return 0;
 }
 
